7c5afe38d61357f5774c1f6756d9ed1466d0be36
Zhe Xiao <zhexiao@163.com>
2016-10-05 19:31:46 -0400

    add api docs

--- docs/api_ref.rst
+++ docs/api_ref.rst
@@ -4,5 +4,12 @@ API Reference
 Converter high-level API
 ------------------------
 
+.. automodule:: vcgif
+    :members:
+    
+    
+Container formats
+-----------------
+
 .. automodule:: vcgif.ZVideo
     :members:
---
+++ vcgif/__init__.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+import os
+
+
+class Converter(object):
+    """
+    Converter class, encapsulates formats and codecs.
+    >>> c = Converter()
+    """
+
+    def __init__(self, ffmpeg_path=None, ffprobe_path=None):
+        """
+        Initialize a new Converter object.
+        """
+        pass
